13e499c98f93bc93d11a5d9a518c886e8da0615a
Avilash <monalisaavi2@gmail.com>
2026-07-25 11:42:39 +0530

    d

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ from fasthtml.common import *
 
 # 1. Initialize the FastHTML application
 app, rt = fast_app()
-
+def log():
+    return Titled(
+        "Login",
+        P("Please enter your credentials to log in:"),
+        Input(type="text", placeholder="Username"),
+        Input(type="password", placeholder="Password"),
+        Button("Login"),
+        Br(),
+        A("Go Back Home", href="/")
+    )
 def cretur():
     return Titled(
         "Create Account",
@@ -12,17 +21,17 @@ def cretur():
         Button("Create Account"),
         A("Go Back Home", href="/")
     )
-
+def home():
+    return Titled(
+        "Welcome to Avilash Login Page",
+        P("This is the homepage of the Avilash Login Page. You can navigate to the Create Account page or the Login page using the links below."),
+        A("Go to Create Account", href="/cret"),
+        A("Go to Login", href="/login")
+    )
 # 2. Define the main homepage route
 @rt("/")
 def get_home():  # Changed name to get_home
-    return Titled(
-        "FastHTML Demo Application",
-        P("Python login Application!"),
-        A("Go to Create Account", href="/cret"),
-        P(), # Adds a small spacing break
-        A("Go to Login", href="/login")
-    )
+    return home()
 
 # 3. Define a second page route
 @rt("/cret")
@@ -31,15 +40,7 @@ def get_cret():  # Changed name to get_cret
 
 @rt("/login")
 def get_login():  # Changed name to get_login
-    return Titled(
-        "Login",
-        P("Please enter your credentials to login:"),
-        Input(type="text", placeholder="Username"),
-        Input(type="password", placeholder="Password"),
-        Button("Login"),
-        Br(),  # Adds a small spacing break
-        A("Go Back Home", href="/")
-    )
+    return log
 
 # 4. Start the server ONLY when running locally
 if __name__ == "__main__":
